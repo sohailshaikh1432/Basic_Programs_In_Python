76ac784134618ab0915888032e193facb5341ed3
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2021-03-31 12:34:04 +0530

    [Sohail]Added : Create stock details management to maintain and access stock detail.

---
+++ OOPs_Programs/stock_account_management.py
@@ -0,0 +1,11 @@
+from Log_Configuration import log_config_file
+from OOPs_Programs.stock_details import StockDetails
+
+logger = log_config_file.get_logger()
+
+if __name__ == '__main__':
+    try:
+        StockDetails().json_handler()
+    except Exception as e:
+        print('Oops! Exception occurred! Please try again!')
+        logger.exception(e)
