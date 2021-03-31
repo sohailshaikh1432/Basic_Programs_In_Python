b2e31f4b958ebb9b4ae34f9a0b0f2fd88951ebd8
sohailshaikh1432 <sohailshaikh1432.ss@gmail.com>
2021-03-31 12:43:51 +0530

    [Sohail]Update : Remove jsonHandler.

--- OOPs_Programs/inventory_details_management.py
+++ OOPs_Programs/inventory_details_management.py
@@ -1,16 +1,11 @@
 from Log_Configuration import log_config_file
-logger = log_config_file.get_logger()
-
 from OOPs_Programs.inventory_details import InventoryDetails
+
+logger = log_config_file.get_logger()
 
 if __name__ == '__main__':
     try:
-        InventoryDetails().json_handler()
+        InventoryDetails()
     except Exception as e:
         print('Oops! Something went wrong! Please try again!')
         logger.exception(e)
-
-
-
-
-
--- OOPs_Programs/stock_account_management.py
+++ OOPs_Programs/stock_account_management.py
@@ -5,7 +5,7 @@ logger = log_config_file.get_logger()
 
 if __name__ == '__main__':
     try:
-        StockDetails().json_handler()
+        StockDetails()
     except Exception as e:
         print('Oops! Exception occurred! Please try again!')
         logger.exception(e)
